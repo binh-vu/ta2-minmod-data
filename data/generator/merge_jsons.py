71119ff28e7049b43046a62bc5e95d20e24963e6
namrata1012 <nsharma4@usc.edu>
2024-01-16 17:18:17 +0530

    Remove extra print statements

--- data/generator/merge_jsons.py
+++ data/generator/merge_jsons.py
@@ -2,18 +2,11 @@ import json
 import sys
 import os
 
-# folder_path = sys.argv[1]
 
 folders = sys.argv[1].split(',') if len(sys.argv) > 1 else []
 merged_json_file = sys.argv[2] if len(sys.argv) > 2 else "merged_inferlink.json"
 
-# merged_json_file = sys.argv[2]
-print(merged_json_file)
-
 list_of_jsons = []
-
-
-# folder_path = sys.argv[1:]
 
 # Check if folders are provided
 if not folders:
@@ -32,23 +25,11 @@ for folder_path in folders:
                 json_data = json.load(file)
 
             if 'MineralSite' in json_data:
-            # Append the 'ABC' key to the merged data
                 list_of_jsons.extend(json_data['MineralSite'])
 
 
-        # Process the JSON data as needed
-        # print(f'Processing {filename}: {json_data}')
 
-# Read JSON from file
-
-# print(list_of_jsons)
-
-# Merge JSONs
 merged_json = {'MineralSite': list_of_jsons}
-
-print(json.dumps(merged_json, indent=2))
-
-# Write merged JSON to a new file
 with open(merged_json_file, 'w') as file:
     json.dump(merged_json, file, indent=2)
 
