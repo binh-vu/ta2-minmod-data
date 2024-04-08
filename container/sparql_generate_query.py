da433a6d76372db5b3b6187c8d5d5b7b35125447
namrata1012 <nsharma4@usc.edu>
2024-04-08 13:27:58 -0700

    Create container for GT model (#110)
    * Fix deposit type format
    
    * Revert "Fix deposit type format"
    
    This reverts commit 7f12f4e98b8e26c0f7b6f649c69ee5756a594a1e.
    
    * Delete code files
    
    * Create dockerfile and readme
    
    ---------
    
    Co-authored-by: Basel Shbita <basel921@gmail.com>

---
+++ container/sparql_generate_query.py
@@ -0,0 +1,56 @@
+import re
+import json
+import requests
+import pandas as pd
+from collections import Counter
+from shapely.wkt import loads
+from shapely.errors import WKTReadingError
+import warnings
+
+warnings.filterwarnings("ignore")
+
+def safe_wkt_load(wkt_string):
+    try:
+        return loads(wkt_string)
+    except WKTReadingError as e:
+        print(f"Error converting WKT: {e}")
+        return None
+    
+def run_sparql_query(query, endpoint='https://minmod.isi.edu/sparql', values=False):
+    # add prefixes
+    final_query = '''
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX : <https://minmod.isi.edu/resource/>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX gkbi: <https://geokb.wikibase.cloud/entity/>
+    PREFIX gkbt: <https://geokb.wikibase.cloud/prop/direct/>
+    PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+    \n''' + query
+    # send query
+    response = requests.post(
+        url=endpoint,
+        data={'query': final_query},
+        headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/sparql-results+json"  # Requesting JSON format
+        },
+        verify=False  # Set to False to bypass SSL verification as per the '-k' in curl
+    )
+    try:
+        qres = response.json()
+        if "results" in qres and "bindings" in qres["results"]:
+            df = pd.json_normalize(qres['results']['bindings'])
+            if values:
+                filtered_columns = df.filter(like='.value').columns
+                df = df[filtered_columns]
+            return df
+    except:
+        return None
+    
+def run_minmod_query(query, values=False):
+    return run_sparql_query(query, endpoint='https://minmod.isi.edu/sparql', values=values)
+
+def run_geokb_query(query, values=False):
+    return run_sparql_query(query, endpoint='https://geokb.wikibase.cloud/query/sparql', values=values)
